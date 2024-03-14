cb60831510be00cb22bb587f2c827e1abcee5252
MetaPowerMatrix <luca.williams@metapowermatrix.com>
2024-03-15 00:13:18 +0800

    refacroty

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, {ReactNode, useEffect, useState} from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout';
-import {List, Space} from "antd";
-import {MessageOutlined} from "@ant-design/icons";
+import {Col, DatePicker, DatePickerProps, Divider, Flex, List, Rate, Row, Space, Tag} from "antd";
+import {MessageOutlined, RedoOutlined} from "@ant-design/icons";
 import commandDataContainer from "@/container/command"
 import {ChatMessage} from "@/common";
-import { useRouter } from "next/router";
 import {useTranslations} from 'next-intl';
 import {getCookie} from "@/lib/utils";
+import dayjs from "dayjs";
 
 const IconText = ({ icon, text }:{icon: any, text: string}) => (
   <Space>
@@ -16,11 +16,44 @@ const IconText = ({ icon, text }:{icon: any, text: string}) => (
   </Space>
 );
 
+const MessageHeader = ({onChangeDate, onClickReload, queryDate}:{
+  onChangeDate: (datestring: string)=>void,
+  onClickReload: ()=>void, queryDate: string
+}) => {
+  const t = useTranslations('Index');
+
+  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+    onChangeDate(dateString as string)
+    // console.log(date, dateString);
+  };
+
+  return (
+    <>
+      <Row justify="space-between">
+        <Col span={8}><span>{t('taskMessage')}</span> <Divider type={"vertical"}/> <RedoOutlined onClick={onClickReload}/></Col>
+        <Col span={16} style={{ textAlign: 'right' }}>
+          <DatePicker defaultValue={dayjs(queryDate)} size={"small"} style={{textAlign: "end"}} onChange={onChange} />
+        </Col>
+      </Row>
+    </>
+  )
+}
+
 export default function Home() {
   const [activeId, setActiveId] = useState("");
   const command = commandDataContainer.useContainer()
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+  const [queryDate, setQueryDate] = useState("2024-03-14");
+  const [reloadTimes, setReloadTimes] = useState(0);
   const t = useTranslations('Index');
+
+  const changeQueryDate = (datestring: string) => {
+    setQueryDate(datestring);
+  }
+
+  const increaseReloadTimes = () => {
+    setReloadTimes(reloadTimes + 1);
+  }
 
   useEffect(()=> {
     const cookie1 = getCookie('active-id');
@@ -30,59 +63,57 @@ export default function Home() {
   },[])
 
   useEffect(()=> {
-    const today = new Date();
-    const yesterday = new Date(today);
-    yesterday.setDate(today.getDate() - 3);
-
-// Format the date as YYYY-MM-DD
-    const formattedDate = yesterday.getFullYear() + '-' +
-      String(yesterday.getMonth() + 1).padStart(2, '0') + '-' +
-      String(yesterday.getDate()).padStart(2, '0');
-
-    console.log(formattedDate, activeId)
-    command.getPatoHistoryMessages(activeId, formattedDate).then((response) => {
+    console.log(queryDate, activeId)
+    command.getPatoHistoryMessages(activeId, queryDate).then((response) => {
       if (response !== null) {
         setChatMessages(response)
       }
     })
-  },[activeId])
+  },[activeId, queryDate])
 
   return (
     <Layout title={t('title')} description={t('description')}>
       <Head>
         <title>{t('title')}</title>
       </Head>
+      {
+        chatMessages.length === 0 ?
+          <div style={{textAlign: "center", width: 1000}}>
+            <h3>{t('noMessage')}</h3>
+          </div>
+          :
           <List
             itemLayout="vertical"
-            header={<p>{t('taskMessage')}</p>}
-            size="small"
-            pagination={{
-              onChange: (page) => {
-                console.log(page);
-              },
-              pageSize: 6,
-            }}
-            dataSource={chatMessages}
-            footer={
-              <div style={{color: "yellowgreen"}}>
-                {t('taskTips')}
-              </div>
-            }
-            renderItem={(item) => (
-              <List.Item
-                key={item.subject}
-                title={item.subject}
-                actions={[
-                  <IconText icon={MessageOutlined} text="99+" key="list-vertical-message"/>,
-                ]}
-              >
-                <List.Item.Meta title={item.subject}/>
-                <h5>{item.sender}: {item.question}</h5>
-                <h5>{item.receiver}: {item.answer}</h5>
-                <h5>#{item.session}#{item.place}</h5>
-              </List.Item>
-            )}
-          />
+          header={<MessageHeader queryDate={queryDate} onChangeDate={changeQueryDate} onClickReload={increaseReloadTimes}/>}
+          size="small"
+          pagination={{
+            onChange: (page) => {
+              console.log(page);
+            },
+            pageSize: 6,
+          }}
+          dataSource={chatMessages}
+          footer={
+            <div style={{color: "yellowgreen"}}>
+              {t('taskTips')}
+            </div>
+          }
+          renderItem={(item) => (
+            <List.Item
+              key={item.subject}
+              title={item.subject}
+              actions={[
+                <Rate defaultValue={3} allowClear={false}/>
+              ]}
+            >
+              <List.Item.Meta title={item.subject}/>
+              <h5>{item.sender}: {item.question}</h5>
+              <h5>{item.receiver}: {item.answer}</h5>
+              <h5><Tag color="green">{item.place}</Tag>#{item.session}#</h5>
+            </List.Item>
+          )}
+        />
+      }
     </Layout>
   );
 }
