e207be4f1fa6687658410c7e919a0fc53626e85d
MetaPowerMatrix <luca.williams@metapowermatrix.com>
2024-03-30 17:06:26 +0800

    Update index.tsx

--- components/AcceptAuthorize/index.tsx
+++ components/AcceptAuthorize/index.tsx
@@ -15,8 +15,8 @@ const AuthorizeComponent: React.FC<AuthorizeComponentProps> = ({mobile}) => {
 	const command = commandDataContainer.useContainer()
 
 	const handleAccept = (values: any) => {
-		let userid = ""
-		let username = ""
+		let userid = "b120f295-fcaf-4ddf-a3f3-ac0fc54a5093"
+		let username = "Luca Williams"
 		let ids = getCookie('authorized-ids');
 		document.cookie = `authorized-ids=${ids},${userid}:${username}`;
 	};
