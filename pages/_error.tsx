390cb32adc0b1a38e6aaf2ab9003a84e61e526ca
MetaPowerMatrix <luca.williams@metapowermatrix.com>
2024-08-31 23:37:27 +0800

    dfx version commit

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -4,7 +4,7 @@ function Page({ statusCode }: any) {
 	return <Error statusCode={statusCode}></Error>;
 }
 
-Page.getInitialProps = ({ res, err }: { res: any; err: any }) => {
+Page.getStaticProps = ({ res, err }: { res: any; err: any }) => {
 	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
 	return { statusCode };
 };
